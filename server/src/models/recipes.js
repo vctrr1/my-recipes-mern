f34f4f829fa35c90d4cd3138802707c2e0253183
Victor Emanoel <victoremaanoel@outlook.com>
2023-10-16 02:33:06 -0300

    Add description to recipesSchema

--- server/src/models/recipes.js
+++ server/src/models/recipes.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 const recipesSchema = mongoose.Schema({
   name: { type: String, required: true },
+  description: { type: String, required: true },
   ingredients: [{ type: String, required: true }], //mongoDB e mongoose identifica que sao um array de ingredientes
   instruction: { type: String, required: true },
   cookingTime: { type: Number, required: true },
-  imageUrl: { type: Number, required: true },
+  imageUrl: { type: String, required: true },
   userOwner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "users",
