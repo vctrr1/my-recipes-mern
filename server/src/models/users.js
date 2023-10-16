1f212cf25dbea03dfa225eb86dc4c7a462de1589
Victor Emanoel <victoremaanoel@outlook.com>
2023-10-16 01:52:26 -0300

    add savedRecipes attribute to users Schema

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   username: { type: String, require: true, unique: true },
   password: { type: String, require: true },
+  savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "recipes" }],
 });
 
 export const userModel = mongoose.model("users", userSchema);
