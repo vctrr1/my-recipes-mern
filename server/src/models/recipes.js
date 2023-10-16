95b6d9c77c1b54197cea95404203615b10af4ae4
Victor Emanoel <victoremaanoel@outlook.com>
2023-10-16 00:15:41 -0300

    Create Recipes Schema mongoose

---
+++ server/src/models/recipes.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const recipesSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  ingredients: [{ type: String, required: true }], //mongoDB e mongoose identifica que sao um array de ingredientes
+  instruction: { type: String, required: true },
+  cookingTime: { type: Number, required: true },
+  imageUrl: { type: Number, required: true },
+  userOwner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+});
+
+export const RecipeModel = mongoose.model("recipes", recipesSchema);
